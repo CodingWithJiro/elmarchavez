003cabd49d1ee08f13e05dbca9f981a984435adf
CodingWithJiro <chavezelmar03@gmail.com>
2026-05-13 01:35:35 +0800

    chore: create initial About component and add it to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from '@/components/sections/about';
 import Hero from '@/components/sections/hero';
 
 export default function Home() {
   return (
     <div className="mx-auto max-w-4xl px-4 py-8">
       <Hero />
+      <About />
     </div>
   );
 }
