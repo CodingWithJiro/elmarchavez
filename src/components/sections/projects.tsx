a05ff72c66053638f14cf01e6f565d9f16fcd47c
CodingWithJiro <chavezelmar03@gmail.com>
2026-05-17 03:32:05 +0800

    feat(projects): add list boilerplate structure for Projects section

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -2,6 +2,15 @@ const Projects = () => {
   return (
     <section>
       <h2>Projects</h2>
+
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+        <li>Project 4</li>
+        <li>Project 5</li>
+        <li>Project 6</li>
+      </ul>
     </section>
   );
 };
