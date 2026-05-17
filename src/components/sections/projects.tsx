5a2303f1dfe775dede7cf7dfda761fa65806d48f
CodingWithJiro <chavezelmar03@gmail.com>
2026-05-18 02:31:14 +0800

    refactor(projects): use list for rendering projects

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -1,15 +1,44 @@
+import Image from 'next/image';
+
+type Project = {
+  id: number;
+  title: string;
+  description: string;
+  imgUrl: string;
+  siteUrl: string;
+};
+
+const projectList: Project[] = [
+  {
+    id: 1,
+    title: 'Where in the World',
+    description: 'Explore countries around the world!',
+    imgUrl: '/projects/where-in-the-world.png',
+    siteUrl: 'https://where-in-the-world-fm-jiro.netlify.app/',
+  },
+];
+
 const Projects = () => {
   return (
     <section>
       <h2>Projects</h2>
 
       <ul>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-        <li>Project 4</li>
-        <li>Project 5</li>
-        <li>Project 6</li>
+        {projectList.map(({ id, title, description, imgUrl, siteUrl }) => {
+          return (
+            <li key={id}>
+              <Image src={imgUrl} alt="" width={1440} height={960} />
+
+              <h3>
+                <a href={siteUrl} target="_blank">
+                  {title}
+                </a>
+              </h3>
+
+              <p>{description}</p>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
