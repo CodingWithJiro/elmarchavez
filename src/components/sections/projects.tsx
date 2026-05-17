6cc924ea7d438906132c398d44b87459f58fae6e
CodingWithJiro <chavezelmar03@gmail.com>
2026-05-18 02:38:50 +0800

    feat(projects): add the rest of the project data in Projects

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -16,6 +16,43 @@ const projectList: Project[] = [
     imgUrl: '/projects/where-in-the-world.png',
     siteUrl: 'https://where-in-the-world-fm-jiro.netlify.app/',
   },
+  {
+    id: 2,
+    title: 'JavaScript Fundamentals',
+    description:
+      'A technical and interactive documentation W3Schools JS Tutorial clone site!',
+    imgUrl: '/projects/javascript-fundamentals.png',
+    siteUrl: 'https://javascript-fundamentals-jiro.netlify.app/',
+  },
+  {
+    id: 3,
+    title: 'Extensions Manager',
+    description:
+      'Filter, remove, and restore your favorite browser extensions!',
+    imgUrl: '/projects/extensions-manager.png',
+    siteUrl: 'https://browser-extension-fm-jiro.netlify.app/',
+  },
+  {
+    id: 4,
+    title: 'No One Asked',
+    description: 'Get a random advice, copy it or share it in X!',
+    imgUrl: '/projects/no-one-asked.png',
+    siteUrl: 'https://no-one-asked-fcc-jiro.netlify.app/',
+  },
+  {
+    id: 5,
+    title: 'Veil and Visions',
+    description: 'Draw three cards to reveal your past, present, and future!',
+    imgUrl: '/projects/veil-and-visions.png',
+    siteUrl: 'https://veil-and-visions-fcc-jiro.netlify.app/',
+  },
+  {
+    id: 6,
+    title: 'Base Apparel',
+    description: 'Sign-up your email, subscribe to Base Apparel!',
+    imgUrl: '/projects/base-apparel.png',
+    siteUrl: 'https://base-apparel-fm-jiro.netlify.app/',
+  },
 ];
 
 const Projects = () => {
