a6e6062c5f409218c62050eca888dbd634a67c3f
CodingWithJiro <chavezelmar03@gmail.com>
2026-05-20 02:11:08 +0800

    feat(styles): add final styles and desktop responsiveness to Projects

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -5,6 +5,7 @@ type Project = {
   title: string;
   description: string;
   imgUrl: string;
+  imgUrlDark: string;
   siteUrl: string;
 };
 
@@ -14,22 +15,23 @@ const projectList: Project[] = [
     title: 'Where in the World',
     description: 'Explore countries around the world!',
     imgUrl: '/projects/where-in-the-world.png',
+    imgUrlDark: '/projects/where-in-the-world-dark.png',
     siteUrl: 'https://where-in-the-world-fm-jiro.netlify.app/',
   },
   {
     id: 2,
     title: 'JavaScript Fundamentals',
-    description:
-      'A technical and interactive documentation W3Schools JS Tutorial clone site!',
+    description: 'Technical W3Schools JS Tutorial clone site!',
     imgUrl: '/projects/javascript-fundamentals.png',
+    imgUrlDark: '/projects/javascript-fundamentals-dark.png',
     siteUrl: 'https://javascript-fundamentals-jiro.netlify.app/',
   },
   {
     id: 3,
     title: 'Extensions Manager',
-    description:
-      'Filter, remove, and restore your favorite browser extensions!',
+    description: 'Manage your favorite browser extensions!',
     imgUrl: '/projects/extensions-manager.png',
+    imgUrlDark: '/projects/extensions-manager-dark.png',
     siteUrl: 'https://browser-extension-fm-jiro.netlify.app/',
   },
   {
@@ -37,6 +39,7 @@ const projectList: Project[] = [
     title: 'No One Asked',
     description: 'Get a random advice, copy it or share it in X!',
     imgUrl: '/projects/no-one-asked.png',
+    imgUrlDark: '/projects/no-one-asked-dark.png',
     siteUrl: 'https://no-one-asked-fcc-jiro.netlify.app/',
   },
   {
@@ -44,6 +47,7 @@ const projectList: Project[] = [
     title: 'Veil and Visions',
     description: 'Draw three cards to reveal your past, present, and future!',
     imgUrl: '/projects/veil-and-visions.png',
+    imgUrlDark: '/projects/veil-and-visions-dark.png',
     siteUrl: 'https://veil-and-visions-fcc-jiro.netlify.app/',
   },
   {
@@ -51,6 +55,7 @@ const projectList: Project[] = [
     title: 'Base Apparel',
     description: 'Sign-up your email, subscribe to Base Apparel!',
     imgUrl: '/projects/base-apparel.png',
+    imgUrlDark: '/projects/base-apparel-dark.png',
     siteUrl: 'https://base-apparel-fm-jiro.netlify.app/',
   },
 ];
@@ -60,31 +65,48 @@ const Projects = () => {
     <section className="border-border/40 bg-card flex flex-col gap-3 rounded-lg border p-4 transition-colors duration-150 ease-in-out">
       <h2 className="text-lg font-bold">Projects</h2>
 
-      <ul className="grid grid-cols-1 gap-y-8 sm:grid-cols-2">
-        {projectList.map(({ id, title, description, imgUrl, siteUrl }) => {
-          return (
-            <li
-              className="flex flex-col items-center justify-center text-center"
-              key={id}
-            >
-              <Image
-                className="border-ring/20 mb-2 max-w-60 rounded-lg border"
-                src={imgUrl}
-                alt=""
-                width={1440}
-                height={960}
-              />
+      <ul className="grid grid-cols-1 gap-y-8 min-[488px]:grid-cols-2 md:grid-cols-3">
+        {projectList.map(
+          ({ id, title, description, imgUrl, imgUrlDark, siteUrl }) => {
+            return (
+              <li
+                className="border-ring/20 focus-within:border-ring has-hover:border-ring bg-muted/40 mx-auto flex max-w-50 flex-col items-center justify-start rounded-lg border text-center transition-all duration-150 ease-in-out md:max-w-57.5"
+                key={id}
+              >
+                <div className="mb-4 h-35.5 w-50 md:w-57.5">
+                  <Image
+                    className="border-ring/20 mb-2 h-full w-full rounded-lg border object-cover dark:hidden"
+                    src={imgUrl}
+                    alt=""
+                    width={1440}
+                    height={960}
+                    loading="eager"
+                  />
+                  <Image
+                    className="border-ring/20 mb-2 hidden h-full w-full rounded-lg border object-cover dark:block"
+                    src={imgUrlDark}
+                    alt={`${title} project preview.`}
+                    width={1440}
+                    height={960}
+                    loading="eager"
+                  />
+                </div>
 
-              <h3 className="mb-1 text-base font-semibold">
-                <a href={siteUrl} target="_blank">
-                  {title}
-                </a>
-              </h3>
+                <h3 className="mb-1 text-base font-semibold">
+                  <a
+                    className="decoration-0 underline-offset-4 opacity-90 hover:underline hover:opacity-100 focus-visible:underline focus-visible:opacity-100 focus-visible:outline-none"
+                    href={siteUrl}
+                    target="_blank"
+                  >
+                    {title}
+                  </a>
+                </h3>
 
-              <p className="text-[0.85rem]">{description}</p>
-            </li>
-          );
-        })}
+                <p className="px-4 pb-4 text-[0.85rem]">{description}</p>
+              </li>
+            );
+          },
+        )}
       </ul>
     </section>
   );
