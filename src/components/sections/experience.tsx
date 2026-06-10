931db09ad67b2be02b9b969a64c34812a9074a61
CodingWithJiro <chavezelmar03@gmail.com>
2026-06-11 03:35:41 +0800

    feat: add work experience data and initial structure

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -1,7 +1,57 @@
+type WorkExperience = {
+  id: number;
+  startDate: string;
+  endDate: string;
+  position: string;
+  companyName: string;
+  location: string;
+};
+
+const workExperiences: WorkExperience[] = [
+  {
+    id: 3,
+    startDate: 'June 2026',
+    endDate: 'Present',
+    position: 'Full Stack Developer',
+    companyName: 'CALEC',
+    location: 'New York, USA (Remote)',
+  },
+  {
+    id: 2,
+    startDate: 'June 2026',
+    endDate: 'Present',
+    position: 'Front End Developer',
+    companyName: 'KeelWorks',
+    location: 'Washington, USA (Remote)',
+  },
+  {
+    id: 1,
+    startDate: 'March 2025',
+    endDate: 'June 2025',
+    position: 'Full Stack Developer',
+    companyName: 'Freelance',
+    location: 'Philippines',
+  },
+];
+
 const Experience = () => {
   return (
     <section>
       <h2>Experience</h2>
+
+      <ul>
+        {workExperiences.map(
+          ({ id, startDate, endDate, position, companyName, location }) => {
+            return (
+              <li key={id}>
+                <p>{position}</p>
+                <p>{`${companyName} · ${location}`}</p>
+                <p>{`${startDate} - ${endDate}`}</p>
+              </li>
+            );
+          },
+        )}
+      </ul>
     </section>
   );
 };
