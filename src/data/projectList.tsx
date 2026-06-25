9b10d940bd4d15300a520ac540a56e4e5c37f7c4
Elmar Chavez <chavezelmar03@gmail.com>
2026-06-26 02:22:00 +0800

    refactor(projects): move projectList to data folder

---
+++ src/data/projectList.tsx
@@ -0,0 +1,36 @@
+import type { Project } from '@/types/project';
+
+export const projectList: Project[] = [
+  {
+    id: 1,
+    title: 'JavaScript Fundamentals',
+    description: 'Technical W3Schools JS Tutorial clone site!',
+    imgUrl: '/projects/javascript-fundamentals.png',
+    imgUrlDark: '/projects/javascript-fundamentals-dark.png',
+    siteUrl: 'https://javascript-fundamentals-jiro.netlify.app/',
+  },
+  {
+    id: 2,
+    title: 'Extensions Manager',
+    description: 'Manage your favorite browser extensions!',
+    imgUrl: '/projects/extensions-manager.png',
+    imgUrlDark: '/projects/extensions-manager-dark.png',
+    siteUrl: 'https://browser-extension-fm-jiro.netlify.app/',
+  },
+  {
+    id: 3,
+    title: 'Veil and Visions',
+    description: 'Draw three cards to reveal your past, present, and future!',
+    imgUrl: '/projects/veil-and-visions.png',
+    imgUrlDark: '/projects/veil-and-visions-dark.png',
+    siteUrl: 'https://veil-and-visions-fcc-jiro.netlify.app/',
+  },
+  {
+    id: 4,
+    title: 'Base Apparel',
+    description: 'Sign-up your email, subscribe to Base Apparel!',
+    imgUrl: '/projects/base-apparel.png',
+    imgUrlDark: '/projects/base-apparel-dark.png',
+    siteUrl: 'https://base-apparel-fm-jiro.netlify.app/',
+  },
+];
