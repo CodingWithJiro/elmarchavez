cc32f921adb8548e0162fafad0ac958b57a46b0b
Elmar Chavez <chavezelmar03@gmail.com>
2026-06-27 23:21:28 +0800

    test(social-links): check every social link has a unique id

--- src/data/social-links.test.ts
+++ src/data/social-links.test.ts
@@ -11,4 +11,9 @@ describe('Social Links data', () => {
     });
     expect(isValid).toBe(true);
   });
+  test('every social link has a unique id', () => {
+    const ids = socialLinks.map(({ id }) => id);
+    const uniqueIds = new Set(ids);
+    expect(socialLinks.length).toBe(uniqueIds.size);
+  });
 });
