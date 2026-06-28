d0f63ae5c1f8030d22fb45f40532a1f738a57c29
Elmar Chavez <chavezelmar03@gmail.com>
2026-06-28 23:35:50 +0800

    test(project-list): check every project has a non-empty title

--- src/data/project-list.test.ts
+++ src/data/project-list.test.ts
@@ -16,7 +16,11 @@ describe('Project List data', () => {
     const imgUrlsDark = projectList.map(({ imgUrlDark }) => imgUrlDark);
     const allImgUrls = [...imgUrls, ...imgUrlsDark];
     const isNotEmpty = allImgUrls.every((imgUrl) => imgUrl.trim().length > 0);
-
+    expect(isNotEmpty).toBe(true);
+  });
+  test('every project has a non-empty title', () => {
+    const titles = projectList.map(({ title }) => title);
+    const isNotEmpty = titles.every((title) => title.trim().length > 0);
     expect(isNotEmpty).toBe(true);
   });
 });
