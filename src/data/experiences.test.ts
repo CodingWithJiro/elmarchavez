9b11686c4a466dc7cd74a32a9310ca6b206a8a33
Elmar Chavez <chavezelmar03@gmail.com>
2026-06-30 08:51:38 +0800

    refactor(experiences): create a helper function for DRYer code

--- src/data/experiences.test.ts
+++ src/data/experiences.test.ts
@@ -1,31 +1,25 @@
 import { workExperiences, certificates } from './experiences';
 
+const isNotEmpty = (items: string[]) => {
+  return items.every((item) => item.trim().length > 0);
+};
+
 describe('Work Experiences and Certificates data', () => {
   test('every work experience has a non-empty position', () => {
     const positions = workExperiences.map(({ position }) => position);
-    const isNotEmpty = positions.every(
-      (position) => position.trim().length > 0,
-    );
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(positions)).toBe(true);
   });
   test('every work experience has a non-empty company name', () => {
     const companyNames = workExperiences.map(({ companyName }) => companyName);
-    const isNotEmpty = companyNames.every(
-      (companyName) => companyName.trim().length > 0,
-    );
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(companyNames)).toBe(true);
   });
   test('every certificate has a non-empty title', () => {
     const titles = certificates.map(({ title }) => title);
-    const isNotEmpty = titles.every((title) => title.trim().length > 0);
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(titles)).toBe(true);
   });
   test('every certificate has a non-empty institution', () => {
     const institutions = certificates.map(({ institution }) => institution);
-    const isNotEmpty = institutions.every(
-      (institution) => institution.trim().length > 0,
-    );
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(institutions)).toBe(true);
   });
   test('every certificate uses a valid HTTPS URL', () => {
     const isValid = certificates.every(({ urlLink }) => {
