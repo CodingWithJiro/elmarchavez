a104fdbf4493e195c3e73af6dc43cf6434eb2dbb
Elmar Chavez <chavezelmar03@gmail.com>
2026-06-30 09:29:17 +0800

    test(project-list): integrate helper functions from utils.ts

--- src/data/project-list.test.ts
+++ src/data/project-list.test.ts
@@ -1,33 +1,23 @@
 import { projectList } from './project-list';
+import { isNotEmpty, isValidUrl } from '@/tests/utils';
 
 describe('Project List data', () => {
   test('every project uses a valid HTTPS URL', () => {
-    const isValid = projectList.every(({ siteUrl }) => {
-      try {
-        return new URL(siteUrl).protocol === 'https:';
-      } catch {
-        return false;
-      }
-    });
-    expect(isValid).toBe(true);
+    const siteUrls = projectList.map(({ siteUrl }) => siteUrl);
+    expect(isValidUrl(siteUrls)).toBe(true);
   });
   test('every project has a non-empty image path', () => {
     const imgUrls = projectList.map(({ imgUrl }) => imgUrl);
     const imgUrlsDark = projectList.map(({ imgUrlDark }) => imgUrlDark);
     const allImgUrls = [...imgUrls, ...imgUrlsDark];
-    const isNotEmpty = allImgUrls.every((imgUrl) => imgUrl.trim().length > 0);
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(allImgUrls)).toBe(true);
   });
   test('every project has a non-empty title', () => {
     const titles = projectList.map(({ title }) => title);
-    const isNotEmpty = titles.every((title) => title.trim().length > 0);
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(titles)).toBe(true);
   });
   test('every project has a non-empty description', () => {
     const descriptions = projectList.map(({ description }) => description);
-    const isNotEmpty = descriptions.every(
-      (description) => description.trim().length > 0,
-    );
-    expect(isNotEmpty).toBe(true);
+    expect(isNotEmpty(descriptions)).toBe(true);
   });
 });
