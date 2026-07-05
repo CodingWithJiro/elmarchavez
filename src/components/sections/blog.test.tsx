27be6bae2d75335daa3669955020a8e014642a34
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-05 23:01:55 +0800

    test(blog): renders every blog article

--- src/components/sections/blog.test.tsx
+++ src/components/sections/blog.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { blogList } from '@/data/blog-list';
 import Blog from './blog';
 
 describe('Blog section', () => {
@@ -6,5 +7,13 @@ describe('Blog section', () => {
     render(<Blog />);
     const title = screen.getByRole('heading', { name: /blog/i });
     expect(title).toBeInTheDocument();
+  });
+  test('renders every blog article', () => {
+    render(<Blog />);
+    const title = screen.getByRole('heading', { name: /blog/i });
+    const section = title.closest('section')!;
+    const blogUl = section.querySelector('ul')!;
+    const blogArticles = blogUl.children;
+    expect(blogArticles).toHaveLength(blogList.length);
   });
 });
