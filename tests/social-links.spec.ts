a5c9faa272a845819ea850689e39e55e18fc948e
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-10 00:06:42 +0800

    test(e2e): visitor can open a footer social link in a new tab

--- tests/social-links.spec.ts
+++ tests/social-links.spec.ts
@@ -1,11 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('visitor can open a hero social link in a new tab', async ({ page }) => {
-  await page.goto('/');
-  const hero = page.getByRole('banner');
-  const link = hero.getByRole('link', { name: /visit my github profile/i });
-  const newPagePromise = page.waitForEvent('popup');
-  await link.click();
-  const newPage = await newPagePromise;
-  await expect(newPage).toHaveURL('https://github.com/CodingWithJiro');
+test.describe('Social Links', () => {
+  test.beforeEach(({ page }) => {
+    page.goto('/');
+  });
+  test('visitor can open a hero social link in a new tab', async ({ page }) => {
+    const hero = page.getByRole('banner');
+    const link = hero.getByRole('link', { name: /visit my github profile/i });
+    const newPagePromise = page.waitForEvent('popup');
+    await link.click();
+    const newPage = await newPagePromise;
+    await expect(newPage).toHaveURL('https://github.com/CodingWithJiro');
+  });
+  test('visitor can open a footer social link in a new tab', async ({
+    page,
+  }) => {
+    const footer = page.getByRole('contentinfo');
+    const link = footer.getByRole('link', { name: /visit my github profile/i });
+    const newPagePromise = page.waitForEvent('popup');
+    await link.click();
+    const newPage = await newPagePromise;
+    await expect(newPage).toHaveURL('https://github.com/CodingWithJiro');
+  });
 });
