9f5c19b7b6e131f3e4e7425eb972e2d128e1c6f7
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-10 22:23:00 +0800

    fix(test): add await and async to page and update locator for hero and footer

--- tests/social-links.spec.ts
+++ tests/social-links.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Social Links', () => {
-  test.beforeEach(({ page }) => {
-    page.goto('/');
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
   });
   test('visitor can open a hero social link in a new tab', async ({ page }) => {
-    const hero = page.getByRole('banner');
+    const hero = page.locator('header');
     const link = hero.getByRole('link', { name: /visit my github profile/i });
     const newPagePromise = page.waitForEvent('popup');
     await link.click();
@@ -15,7 +15,7 @@ test.describe('Social Links', () => {
   test('visitor can open a footer social link in a new tab', async ({
     page,
   }) => {
-    const footer = page.getByRole('contentinfo');
+    const footer = page.locator('footer');
     const link = footer.getByRole('link', { name: /visit my github profile/i });
     const newPagePromise = page.waitForEvent('popup');
     await link.click();
