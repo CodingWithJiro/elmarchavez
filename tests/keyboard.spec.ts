4f04423da36c0b6b7c3ffa5396aadd87d7b7f5dc
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-10 19:18:12 +0800

    test(e2e): visitor can tab to a footer social link and open in a new tab

--- tests/keyboard.spec.ts
+++ tests/keyboard.spec.ts
@@ -62,4 +62,21 @@ test.describe('Keyboard Accessibility', () => {
     const newPage = await newPagePromise;
     await expect(newPage).toHaveURL('https://github.com/CodingWithJiro');
   });
+  test('visitor can tab to a footer social link and open in a new tab', async ({
+    page,
+    browserName,
+  }) => {
+    test.skip(
+      browserName === 'webkit',
+      'WebKit on Windows does not traverse focus order correctly.',
+    );
+    const hero = page.locator('footer');
+    const link = hero.getByRole('link', { name: /visit my github profile/i });
+    await tabUntilFocused(page, link, 'GitHub Link');
+    await expect(link).toBeFocused();
+    const newPagePromise = page.waitForEvent('popup');
+    await page.keyboard.press('Enter');
+    const newPage = await newPagePromise;
+    await expect(newPage).toHaveURL('https://github.com/CodingWithJiro');
+  });
 });
