b3c557daa81fbbcbf0ba4e74cae9517f095a400e
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-16 19:23:09 +0800

    feat(style): adjust flex spacing in Experience section

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -3,16 +3,16 @@ import { workExperiences, certificates } from '@/data/experiences';
 
 const Experience = () => {
   return (
-    <section className="border-border/40 bg-card flex shrink-0 flex-col gap-6 rounded-lg border p-4 transition-colors duration-150 ease-in-out min-[488px]:flex-row md:max-w-[clamp(16rem,-12rem+50vw,20rem)] md:flex-col">
-      <article className="flex-1">
+    <section className="border-border/40 bg-card flex shrink-0 flex-col gap-4 rounded-lg border p-4 transition-colors duration-150 ease-in-out min-[488px]:flex-row md:max-w-[clamp(16rem,-12rem+50vw,20rem)] md:flex-col md:gap-8">
+      <article>
         <h2 className="mb-3 text-lg font-bold">Experience</h2>
 
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 md:gap-8">
           {workExperiences.map(
             ({ id, startDate, endDate, position, companyName, location }) => {
               return (
                 <li
-                  className="before:border-border before:bg-background first:before:bg-foreground after:bg-border/50 hover:before:bg-foreground relative pl-6 before:absolute before:top-1 before:left-0 before:h-3 before:w-3 before:rounded-full before:border-2 before:transition-colors before:duration-150 before:ease-in-out after:absolute after:top-4 after:-bottom-4 after:left-1.5 after:w-px last:after:hidden"
+                  className="before:border-border before:bg-background first:before:bg-foreground after:bg-border/50 hover:before:bg-foreground relative pl-4.5 before:absolute before:top-1 before:left-0 before:h-3 before:w-3 before:rounded-full before:border-2 before:transition-colors before:duration-150 before:ease-in-out after:absolute after:top-4 after:-bottom-6 after:left-1.5 after:w-px last:after:hidden md:pl-6 md:after:-bottom-10"
                   key={id}
                 >
                   <p className="mb-1 text-base font-semibold">{position}</p>
@@ -25,15 +25,15 @@ const Experience = () => {
         </ul>
       </article>
 
-      <article className="flex-1">
+      <article>
         <h2 className="mb-3 text-lg font-bold">Certificates</h2>
 
-        <ul className="flex flex-col gap-3">
+        <ul className="flex flex-col gap-4">
           {certificates.map(
             ({ id, title, institution, dateReceived, urlLink }) => {
               return (
                 <li
-                  className="before:border-border before:bg-background first:before:bg-foreground after:bg-border/50 hover:before:bg-foreground relative pl-6 before:absolute before:top-1 before:left-0 before:h-3 before:w-3 before:rounded-full before:border-2 before:transition-colors before:duration-150 before:ease-in-out after:absolute after:top-4 after:-bottom-4 after:left-1.5 after:w-px after:transition-colors after:duration-150 after:ease-in-out last:after:hidden"
+                  className="before:border-border before:bg-background first:before:bg-foreground after:bg-border/50 hover:before:bg-foreground relative pl-4.5 before:absolute before:top-1 before:left-0 before:h-3 before:w-3 before:rounded-full before:border-2 before:transition-colors before:duration-150 before:ease-in-out after:absolute after:top-4 after:-bottom-6 after:left-1.5 after:w-px after:transition-colors after:duration-150 after:ease-in-out last:after:hidden md:pl-6"
                   key={id}
                 >
                   <a
@@ -45,9 +45,8 @@ const Experience = () => {
                     {title}
                     <FiExternalLink className="group-focus-visible:text-foreground text-muted-foreground group-hover:text-foreground absolute top-px -right-5" />
                   </a>
-                  <p className="text-[0.75rem]">
-                    {institution}
-                    {' · '}
+                  <p className="flex flex-col text-[0.75rem]">
+                    <span>{institution}</span>
                     <span className="text-muted-foreground text-[0.70rem]">
                       {`Issued ${dateReceived}`}
                     </span>
