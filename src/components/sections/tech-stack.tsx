6dc8cd39f9440512f48f3cd37b2922a120cd46f9
Elmar Chavez <chavezelmar03@gmail.com>
2026-07-16 22:01:50 +0800

    feat: add DevOps in Tech Stack section

--- src/components/sections/tech-stack.tsx
+++ src/components/sections/tech-stack.tsx
@@ -5,7 +5,8 @@ export const FRONTEND: string[] = [
   'Next.js',
   'Tailwind CSS',
 ];
-export const BACKEND: string[] = ['Node.js', 'Express', 'PostgreSQL'];
+export const BACKEND: string[] = ['Node.js', 'Express.js', 'PostgreSQL'];
+export const DEVOPS: string[] = ['GitHub Actions', 'Docker'];
 
 const TechStack = () => {
   return (
@@ -34,6 +35,23 @@ const TechStack = () => {
 
         <ul className="gap flex flex-wrap gap-x-2 gap-y-1">
           {BACKEND.map((tech) => {
+            return (
+              <li
+                className="bg-muted/40 border-ring/20 rounded-sm border px-2 py-1 text-[0.85rem]"
+                key={tech}
+              >
+                {tech}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      <section>
+        <h3 className="mb-1 text-base font-semibold">DevOps</h3>
+
+        <ul className="gap flex flex-wrap gap-x-2 gap-y-1">
+          {DEVOPS.map((tech) => {
             return (
               <li
                 className="bg-muted/40 border-ring/20 rounded-sm border px-2 py-1 text-[0.85rem]"
