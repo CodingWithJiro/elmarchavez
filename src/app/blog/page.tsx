ca65efe0fa4c60f19694dde35f979a909b732105
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-15 02:54:21 +0800

    feat(blog): copy the blog list format from home page to blog page as initial structure

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import Footer from '@/components/sections/footer';
 import ThemeToggle from '@/components/theme/theme-toggle';
+import { BLOGS } from '@/data/blog-list';
 
 const metaDescription =
   'Blog posts on full stack development and tech from Elmar Chavez.';
@@ -26,6 +29,8 @@ export const metadata: Metadata = {
   },
 };
 
+const blogsLatestOrder = BLOGS.slice().reverse();
+
 export default function BlogPage() {
   return (
     <main id="main" className="mx-auto max-w-130 px-4 py-8 md:max-w-4xl">
@@ -39,6 +44,63 @@ export default function BlogPage() {
 
         <ThemeToggle />
       </header>
+
+      <ul>
+        {blogsLatestOrder.map(
+          ({
+            id,
+            title,
+            description,
+            publishDate,
+            readTime,
+            tags,
+            blogUrl,
+            imgUrl,
+          }) => {
+            return (
+              <li
+                className="border-border/60 border-b pb-4 last:border-b-0 md:flex md:flex-row md:items-center md:justify-between md:gap-4"
+                key={id}
+              >
+                <article>
+                  <h2 className="mb-2 text-[0.875rem] font-semibold">
+                    {title}
+                  </h2>
+
+                  <p className="mb-1 text-[0.65rem] font-light">
+                    {publishDate} • {readTime}
+                  </p>
+
+                  <p className="mb-2 text-[0.75rem] font-normal">
+                    {description}
+                  </p>
+
+                  <div className="flex flex-col gap-4 pr-4 min-[425px]:flex-row min-[425px]:items-center min-[425px]:justify-between">
+                    <ul className="flex gap-2 text-[0.65rem] font-light">
+                      {tags.map((tag) => {
+                        return <li key={tag}>{tag}</li>;
+                      })}
+                    </ul>
+
+                    <Link
+                      className="focus-visible:outline-muted-foreground w-fit rounded-sm text-[0.75rem] font-normal underline-offset-4 outline-2 outline-offset-4 outline-transparent transition-colors duration-150 ease-in-out hover:underline md:text-right"
+                      href={blogUrl}
+                      target="_blank"
+                      aria-label={`Read more about ${title}`}
+                    >
+                      Read More →
+                    </Link>
+                  </div>
+                </article>
+
+                <div className="hidden w-67.5 shrink-0 md:block">
+                  <Image src={imgUrl} alt="" width={800} height={400} />
+                </div>
+              </li>
+            );
+          },
+        )}
+      </ul>
 
       <Footer />
     </main>
