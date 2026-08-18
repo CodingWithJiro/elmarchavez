7563d883f143ae36bbf64a3f92f33f4e4bb2c4f6
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-18 17:45:21 +0800

    refactor(blog): use getBlog helper function and add show an error component for no blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import ThemeToggle from '@/components/theme/theme-toggle';
-import { BLOGS } from '@/data/blog-list';
-import { getSlug } from '@/lib/utils';
+import Link from 'next/link';
+import { getBlog } from '@/lib/utils';
 
 type ArticlePageProps = {
   params: Promise<{ slug: string }>;
@@ -8,18 +8,33 @@ type ArticlePageProps = {
 
 export default async function ArticlePage({ params }: ArticlePageProps) {
   const { slug } = await params;
-  const blog = BLOGS.find(({ blogUrl }) => blogUrl.endsWith(getSlug(slug)))!;
+  const blog = getBlog(slug);
+
+  if (!blog) {
+    return (
+      <main id="main" className="mx-auto max-w-130 px-4 py-8 md:max-w-4xl">
+        <header className="flex justify-between">
+          <Link href="/blog">← Back to Blog</Link>
+
+          <ThemeToggle />
+        </header>
+
+        <h1 className="mb-1 text-lg font-bold md:text-3xl">Blog not found.</h1>
+      </main>
+    );
+  }
+
   const { title, publishDate, readTime, tags, imgUrl, imgAlt } = blog;
 
   return (
     <main id="main" className="mx-auto max-w-130 px-4 py-8 md:max-w-4xl">
       <header className="flex justify-between">
-        <div>
-          <h1 className="mb-1 text-lg font-bold md:text-3xl">{title}</h1>
-        </div>
+        <Link href="/blog">← Back to Blog</Link>
 
         <ThemeToggle />
       </header>
+
+      <h1 className="mb-1 text-lg font-bold md:text-3xl">{title}</h1>
     </main>
   );
 }
