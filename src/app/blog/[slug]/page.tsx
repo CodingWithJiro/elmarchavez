30a379c1dfed74743659c02f87f72db48ba3c31b
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-22 00:33:46 +0800

    feat(blog): update article slug page to use MDX files for articles

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,6 @@
-import { getBlog } from '@/lib/utils';
+import { getBlogMeta, getBlogArticle } from '@/lib/utils';
 import { BreadcrumbItem } from '@/types/breadcrumb-item';
 import ArticleHeader from '@/components/sections/article-header';
-import Image from 'next/image';
 
 type ArticlePageProps = {
   params: Promise<{ slug: string }>;
@@ -9,7 +8,7 @@ type ArticlePageProps = {
 
 export default async function ArticlePage({ params }: ArticlePageProps) {
   const { slug } = await params;
-  const blog = getBlog(slug);
+  const blog = getBlogMeta(slug);
   const breadcrumbItems: BreadcrumbItem[] = [
     {
       label: 'Blog',
@@ -30,33 +29,18 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
     );
   }
 
-  const { title, publishDate, readTime, tags, imgUrl, imgAlt } = blog;
+  const Article = await getBlogArticle(blog.id);
 
   return (
     <main id="main" className="mx-auto max-w-130 px-4 py-8 md:max-w-2xl">
-      <ArticleHeader breadcrumbItems={breadcrumbItems} currentLabel={title} />
+      <ArticleHeader
+        breadcrumbItems={breadcrumbItems}
+        currentLabel={blog.title}
+      />
 
-      <h1 className="mb-1 text-lg font-bold md:text-3xl">{title}</h1>
-
-      <p>{publishDate}</p>
-
-      <p>{readTime}</p>
-
-      <ul className="flex gap-2 text-[0.65rem] font-light">
-        {tags.map((tag) => {
-          return <li key={tag}>{tag}</li>;
-        })}
-      </ul>
-
-      <div className="w-100">
-        <Image
-          src={imgUrl}
-          alt={imgAlt}
-          width={800}
-          height={400}
-          className="aspect-video w-full object-cover"
-        />
-      </div>
+      <article className="prose prose-invert max-w-none">
+        <Article />
+      </article>
     </main>
   );
 }
