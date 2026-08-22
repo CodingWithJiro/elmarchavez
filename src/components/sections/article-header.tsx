91ebe4c80b038b960b5cca22b343e6cee62bf03e
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-23 00:09:39 +0800

    feat(blog): move blog meta data to ArticleHeader component unstyled

--- src/components/sections/article-header.tsx
+++ src/components/sections/article-header.tsx
@@ -1,21 +1,62 @@
 import Breadcrumb from './breadcrumb';
 import ThemeToggle from '../theme/theme-toggle';
 import { BreadcrumbItem } from '@/types/breadcrumb-item';
+import { BlogType } from '@/types/blog';
+import Image from 'next/image';
 
 type ArticleHeaderProps = {
   breadcrumbItems: BreadcrumbItem[];
-  currentLabel: string;
+  blogMeta: BlogType;
 };
 
-const ArticleHeader = ({
-  breadcrumbItems,
-  currentLabel,
-}: ArticleHeaderProps) => {
-  return (
-    <header className="mb-12 flex items-center justify-between">
-      <Breadcrumb items={breadcrumbItems} currentLabel={currentLabel} />
+const ArticleHeader = ({ breadcrumbItems, blogMeta }: ArticleHeaderProps) => {
+  if (blogMeta.id === -99) {
+    return (
+      <header className="mb-12">
+        <div className="mb-12 flex items-center justify-between">
+          <Breadcrumb items={breadcrumbItems} currentLabel={blogMeta.title} />
+          <ThemeToggle />
+        </div>
 
-      <ThemeToggle />
+        <h1 className="mb-8 text-[36px] leading-10 font-extrabold">
+          {blogMeta.title}
+        </h1>
+      </header>
+    );
+  }
+
+  return (
+    <header className="mb-12">
+      <div className="mb-12 flex items-center justify-between">
+        <Breadcrumb items={breadcrumbItems} currentLabel={blogMeta.title} />
+        <ThemeToggle />
+      </div>
+
+      <section>
+        <Image
+          src={blogMeta.imgUrl}
+          alt={blogMeta.imgAlt}
+          width={800}
+          height={400}
+          className="mb-8"
+        />
+
+        <h1 className="mb-8 text-[36px] leading-10 font-extrabold">
+          {blogMeta.title}
+        </h1>
+
+        <p>Elmar Chavez</p>
+
+        <p className="mb-1 text-[0.65rem] font-light">
+          {blogMeta.publishDate} • {blogMeta.readTime}
+        </p>
+
+        <ul className="mb-2 flex gap-2 text-[0.65rem] font-light">
+          {blogMeta.tags.map((tag) => {
+            return <li key={tag}>{tag}</li>;
+          })}
+        </ul>
+      </section>
     </header>
   );
 };
