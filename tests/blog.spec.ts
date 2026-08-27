10091007cc02573a325556b344604a084761f153
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-27 23:49:48 +0800

    test(blog): update test from checking if read more goes to new tab to if blog article link has correct href

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,15 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { blogList } from '@/data/blog-list';
 
-test('visitor can open a blog link in a new tab', async ({ page }) => {
+test('blog article link has correct href', async ({ page }) => {
   await page.goto('/');
   const blog = blogList[0];
   expect(blog).toBeDefined();
   const link = page.getByRole('link', {
     name: `Read more about ${blog.title}`,
   });
-  const newPagePromise = page.waitForEvent('popup');
-  await link.click();
-  const newPage = await newPagePromise;
-  await expect(newPage).toHaveURL(blog.blogUrl);
+  await expect(link).toHaveAttribute('href', blog.blogUrl);
 });
