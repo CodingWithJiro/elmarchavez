4cf67104d2a83b7923ba95748e1ee6a313d92a45
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-28 19:31:14 +0800

    test(e2e): visitor can view the Blog page

--- tests/blog.spec.ts
+++ tests/blog.spec.ts
@@ -1,12 +1,25 @@
 import { test, expect } from '@playwright/test';
 import { blogList } from '@/data/blog-list';
 
-test('blog article link has correct href', async ({ page }) => {
-  await page.goto('/');
-  const blog = blogList[0];
-  expect(blog).toBeDefined();
-  const link = page.getByRole('link', {
-    name: `Read more about ${blog.title}`,
+test.describe('Blog', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
   });
-  await expect(link).toHaveAttribute('href', blog.blogUrl);
+  test('visitor can see the Blog section', async ({ page }) => {
+    const heading = page.getByRole('heading', { name: /^blog$/i });
+    await expect(heading).toBeVisible();
+  });
+  test('blog article link has correct href', async ({ page }) => {
+    const blog = blogList[0];
+    expect(blog).toBeDefined();
+    const link = page.getByRole('link', {
+      name: `Read more about ${blog.title}`,
+    });
+    await expect(link).toHaveAttribute('href', blog.blogUrl);
+  });
+  test('visitor can view the Blog page', async ({ page }) => {
+    await page.goto('/blog');
+    const heading = page.getByRole('heading', { name: /^blog$/i });
+    await expect(heading).toBeVisible();
+  });
 });
