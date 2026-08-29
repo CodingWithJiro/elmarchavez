334439017c6af6554846c82bc804b16a9f346c5c
Elmar Chavez <chavezelmar03@gmail.com>
2026-08-30 02:13:49 +0800

    test(e2e): visitor can see the Project section

--- tests/projects.spec.ts
+++ tests/projects.spec.ts
@@ -1,17 +1,24 @@
 import { test, expect } from '@playwright/test';
 import { projectList } from '@/data/project-list';
 
-test('visitor can open a project in a new tab', async ({ page }) => {
-  await page.goto('/');
-  const link = page.getByRole('link', {
-    name: /javascript fundamentals/i,
+test.describe('Projects', () => {
+  test('visitor can see the Project section', async ({ page }) => {
+    await page.goto('/');
+    const heading = page.getByRole('heading', { name: /^projects$/i });
+    await expect(heading).toBeVisible();
   });
-  const project = projectList.find(
-    ({ title }) => title === 'JavaScript Fundamentals',
-  )!;
-  expect(project).toBeDefined();
-  const newPagePromise = page.waitForEvent('popup');
-  await link.click();
-  const newPage = await newPagePromise;
-  await expect(newPage).toHaveURL(project.siteUrl);
+  test('visitor can open a project in a new tab', async ({ page }) => {
+    await page.goto('/');
+    const link = page.getByRole('link', {
+      name: /javascript fundamentals/i,
+    });
+    const project = projectList.find(
+      ({ title }) => title === 'JavaScript Fundamentals',
+    )!;
+    expect(project).toBeDefined();
+    const newPagePromise = page.waitForEvent('popup');
+    await link.click();
+    const newPage = await newPagePromise;
+    await expect(newPage).toHaveURL(project.siteUrl);
+  });
 });
