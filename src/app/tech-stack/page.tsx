8bff087873c87f2a702efcf4b81f8e8ff64477a3
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-08 00:08:37 +0800

    feat(tech-stack): create the initial tech stack page

---
+++ src/app/tech-stack/page.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from 'next';
+import Breadcrumb from '@/components/sections/breadcrumb';
+import ThemeToggle from '@/components/theme/theme-toggle';
+import Footer from '@/components/sections/footer';
+
+const metaDescription =
+  'List of all the technologies Elmar Chavez have experience with.';
+
+export const metadata: Metadata = {
+  title: 'Elmar Chavez | Tech Stack',
+  description: metaDescription,
+  keywords: [
+    'Elmar Chavez Tech Stack',
+    'Web Development Technologies',
+    'Full Stack Development Skills',
+    'Frontend',
+    'Backend',
+  ],
+  openGraph: {
+    title: 'Tech Stack | Elmar Chavez',
+    description: metaDescription,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Tech Stack | Elmar Chavez',
+    description: metaDescription,
+  },
+};
+
+export default function TechStackPage() {
+  return (
+    <main
+      id="main"
+      className="mx-auto min-h-screen max-w-130 px-4 py-8 md:max-w-4xl"
+    >
+      <header className="mb-8 flex items-center justify-between">
+        <Breadcrumb items={[]} currentLabel="Tech Stack" />
+        <ThemeToggle />
+      </header>
+
+      <section>
+        <h1 className="mb-1 text-lg font-bold md:text-3xl">Tech Stack</h1>
+        <p className="mb-6 text-sm font-medium md:text-base">
+          List of technologies I used so far.
+        </p>
+      </section>
+
+      <p>Add list here.</p>
+
+      <Footer />
+    </main>
+  );
+}
