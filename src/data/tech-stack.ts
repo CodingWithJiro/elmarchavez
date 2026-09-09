a59fe219e8940d132a5138675e2a2b588bb2cd76
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-09 23:22:49 +0800

    feat(tech-stack): create initial tech stack data

---
+++ src/data/tech-stack.ts
@@ -0,0 +1,9 @@
+export const FRONTEND: string[] = [
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Tailwind CSS',
+];
+export const BACKEND: string[] = ['Node.js', 'Express.js', 'PostgreSQL'];
+export const DEVOPS: string[] = ['GitHub Actions', 'Docker'];
