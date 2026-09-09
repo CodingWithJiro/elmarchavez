7da8e16a959778b9d95c54ff4562a1108664dbd5
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-10 02:10:22 +0800

    refactor(view-all-link): remove color transition

--- src/components/ui/view-all-link.tsx
+++ src/components/ui/view-all-link.tsx
@@ -8,10 +8,10 @@ type ViewAllLinkProps = {
 const ViewAllLink = ({ href }: ViewAllLinkProps) => {
   return (
     <Link
-      className="group focus-visible:outline-muted-foreground flex items-center gap-1 rounded-sm text-[1rem] font-normal outline-2 outline-offset-4 outline-transparent transition-all duration-150 ease-in-out"
+      className="group focus-visible:outline-muted-foreground flex items-center gap-1 rounded-sm text-[1rem] font-normal outline-2 outline-offset-4 outline-transparent"
       href={href}
     >
-      <span className="max-w-0 overflow-hidden text-[0.75rem] whitespace-nowrap underline-offset-4 opacity-0 transition-all duration-150 group-hover:max-w-xs group-hover:underline group-hover:opacity-100 group-focus:max-w-xs group-focus:opacity-100">
+      <span className="max-w-0 overflow-hidden text-[0.75rem] whitespace-nowrap underline-offset-4 opacity-0 group-hover:max-w-xs group-hover:underline group-hover:opacity-100 group-focus:max-w-xs group-focus:opacity-100">
         View all
       </span>
       <span className="group-hover:hidden group-focus:hidden">
