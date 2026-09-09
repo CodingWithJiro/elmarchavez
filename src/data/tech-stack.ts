2f62b828627e0c4b10be0ea82f882de46ef223ef
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-10 00:58:42 +0800

    feat(tech-stack): complete list of tech used so far and export landing page data

--- src/data/tech-stack.ts
+++ src/data/tech-stack.ts
@@ -4,6 +4,33 @@ export const FRONTEND: string[] = [
   'React',
   'Next.js',
   'Tailwind CSS',
+  'React Router',
+  'Styled Components',
+  'HTML',
+  'CSS',
+  'Vite',
 ];
 export const BACKEND: string[] = ['Node.js', 'Express.js', 'PostgreSQL'];
+export const TESTING_AND_QUALITY: string[] = [
+  'Vitest',
+  'React Testing Library',
+  'MSW',
+  'Playwright',
+  'Axe DevTools',
+  'ESLint',
+  'Prettier',
+];
 export const DEVOPS: string[] = ['GitHub Actions', 'Docker'];
+export const CONTENT: string[] = ['MDX', 'Tailwind CSS Typography'];
+export const TOOLS: string[] = [
+  'Git',
+  'GitHub',
+  'VS Code',
+  'Slack',
+  'Discord',
+  'Teams',
+];
+
+export const frontend = FRONTEND.slice(0, 5);
+export const backend = BACKEND.slice(0, 5);
+export const devOps = DEVOPS.slice(0, 5);
