69deb1002cb257709aa1d3d704a9b3a009b3e82f
Elmar Chavez <chavezelmar03@gmail.com>
2026-09-12 12:38:54 +0800

    perf(skip-link): use Link instead of anchor tag

--- src/components/accessibility/skip-link.tsx
+++ src/components/accessibility/skip-link.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 const SkipLink = () => {
   return (
-    <a
+    <Link
       className="focus-visible:outline-muted-foreground focus-visible:text-foreground focus-visible:bg-card focus-visible:border-border/40 sr-only rounded-sm text-sm outline-offset-4 focus-visible:not-sr-only focus-visible:absolute focus-visible:top-4 focus-visible:left-4 focus-visible:border focus-visible:p-2 focus-visible:outline-2"
       href="#main"
     >
       Skip to main content
-    </a>
+    </Link>
   );
 };
 
